933267b9926d1994f81a797ea9516d41f4c38a6a
ghkt741 <ghkt741@K1076.gmkt.inc>
2019-09-09 16:42:41 +0900

    rest-api 프로젝트 생성(boot - gradle)

---
+++ src/main/java/com/asset/webservice/BootTestProject2Application.java
@@ -0,0 +1,13 @@
+package com.asset.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTestProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTestProject2Application.class, args);
+	}
+
+}
